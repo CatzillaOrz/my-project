e2b454aad3c17288d989cf0080864cebb324a217
catzillaorz <catzillaorz@outlook.com>
2017-11-17 12:02:30 +0800

    VuetablePagination

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <div class="ui container">
-      <my-vuetable></my-vuetable>
-    </div>
+    <my-vuetable></my-vuetable>
     <!-- <router-view/> -->
   </div>
 </template>
 
 <script>
-import MyVuetable from './components/MyVuetable'
-
-export default {
-  name: 'app',
-  components: {
-    MyVuetable
+  import MyVuetable from './components/MyVuetable'
+  
+  export default {
+    name: 'app',
+    components: {
+      MyVuetable
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/components/MyVuetable.vue
+++ src/components/MyVuetable.vue
@@ -1,66 +1,73 @@
 <template>
-  <vuetable ref="vuetable"
-    api-url="https://vuetable.ratiw.net/api/users"
-    :fields="fields"
-  ></vuetable>
+  <div class="ui container">
+    <vuetable ref="vuetable" api-url="https://vuetable.ratiw.net/api/users" :fields="fields" pagination-path="" @vuetable:pagination-data="onPaginationData"></vuetable>
+    <vuetable-pagination ref="pagination" @vuetable-pagination:change-page="onChangePage"></vuetable-pagination>
+  </div>
 </template>
 
 <script>
-import Vuetable from 'vuetable-2/src/components/Vuetable'
-import accounting from 'accounting'
-import moment from 'moment'
-
-
-export default {
-  components: {
-    Vuetable
-  },
-  data () {
-    return {
-      fields: [
-        'name', 'email',
-        {
-          name: 'birthdate',
-          titleClass: 'center aligned',
-          dataClass: 'center aligned',
-          callback: 'formatDate|DD-MM-YYYY'
-        },
-        {
-          name: 'nickname',
-          callback: 'allcap'
-        },
-        {
-          name: 'gender',
-          titleClass: 'center aligned',
-          dataClass: 'center aligned',
-          callback: 'genderLabel'
-        },
-        {
-          name: 'salary',
-          titleClass: 'center aligned',
-          dataClass: 'right aligned',
-          callback: 'formatNumber'
-        }
-      ]
+  import Vuetable from "vuetable-2/src/components/Vuetable";
+  import accounting from "accounting";
+  import moment from "moment";
+  // import VuetablePagination from 'vuetable-2/src/components/VuetablePaginationDropdown'
+  import VuetablePagination from "vuetable-2/src/components/VuetablePagination";
+  
+  export default {
+    components: {
+      Vuetable,
+      VuetablePagination
+    },
+    data() {
+      return {
+        fields: [
+          "name",
+          "email",
+          {
+            name: "birthdate",
+            titleClass: "center aligned",
+            dataClass: "center aligned",
+            callback: "formatDate|DD-MM-YYYY"
+          },
+          {
+            name: "nickname",
+            callback: "allcap"
+          },
+          {
+            name: "gender",
+            titleClass: "center aligned",
+            dataClass: "center aligned",
+            callback: "genderLabel"
+          },
+          {
+            name: "salary",
+            titleClass: "center aligned",
+            dataClass: "right aligned",
+            callback: "formatNumber"
+          }
+        ]
+      };
+    },
+    methods: {
+      allcap(value) {
+        return value.toUpperCase();
+      },
+      genderLabel(value) {
+        return value === "M" ?
+          '<span class="ui teal label"><i class="large man icon"></i>Male</span>' :
+          '<span class="ui pink label"><i class="large woman icon"></i>Female</span>';
+      },
+      formatNumber(value) {
+        return accounting.formatNumber(value, 2);
+      },
+      formatDate(value, fmt = "D MMM YYYY") {
+        return value == null ? "" : moment(value, "YYYY-MM-DD").format(fmt);
+      },
+      onPaginationData(paginationData) {
+        this.$refs.pagination.setPaginationData(paginationData)
+      },
+      onChangePage(page) {
+        this.$refs.vuetable.changePage(page)
+      }
     }
-  },
-  methods: {
-    allcap (value) {
-      return value.toUpperCase()
-    },
-    genderLabel (value) {
-      return value === 'M'
-        ? '<span class="ui teal label"><i class="large man icon"></i>Male</span>'
-        : '<span class="ui pink label"><i class="large woman icon"></i>Female</span>'
-    },
-    formatNumber (value) {
-        return accounting.formatNumber(value, 2)
-    },
-    formatDate (value, fmt = 'D MMM YYYY') {
-      return (value == null)
-        ? ''
-        : moment(value, 'YYYY-MM-DD').format(fmt)
-    }
-  }
-}
+  };
 </script>
