d162924c8a0cd97a50b681a5ced5532a9f5c7122
catzillaorz <catzillaorz@outlook.com>
2017-11-18 18:08:13 +0800

    format code

--- src/components/MyVuetable.vue
+++ src/components/MyVuetable.vue
@@ -1,16 +1,16 @@
 <script>
-import accounting from 'accounting'
-import moment from 'moment'
-import Vue from 'vue'
-import VueEvents from 'vue-events'
-import Vuetable from 'vuetable-2/src/components/Vuetable'
-import VuetablePagination from 'vuetable-2/src/components/VuetablePagination'
-import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo'
-import CustomActions from './CustomActions'
-import FilterBar from './FilterBar'
-Vue.use(VueEvents)
-Vue.component('custom-actions', CustomActions)
-Vue.component('filter-bar', FilterBar)
+import accounting from "accounting";
+import moment from "moment";
+import Vue from "vue";
+import VueEvents from "vue-events";
+import Vuetable from "vuetable-2/src/components/Vuetable";
+import VuetablePagination from "vuetable-2/src/components/VuetablePagination";
+import VuetablePaginationInfo from "vuetable-2/src/components/VuetablePaginationInfo";
+import CustomActions from "./CustomActions";
+import FilterBar from "./FilterBar";
+Vue.use(VueEvents);
+Vue.component("custom-actions", CustomActions);
+Vue.component("filter-bar", FilterBar);
 export default {
   components: {
     Vuetable,
@@ -29,115 +29,114 @@ export default {
     sortOrder: {
       type: Array,
       default() {
-        return []
+        return [];
       }
     },
     appendParams: {
       type: Object,
       default() {
-        return {}
+        return {};
       }
     },
     detailRowComponent: {
       type: String
     }
   },
-  data () {
-    return {}
+  data() {
+    return {};
   },
-  mounted () {
-    this.$events.$on('filter-set', eventData => this.onFilterSet(eventData))
-    this.$events.$on('filter-reset', e => this.onFilterReset())
+  mounted() {
+    this.$events.$on("filter-set", eventData => this.onFilterSet(eventData));
+    this.$events.$on("filter-reset", e => this.onFilterReset());
   },
   render(h) {
     return h(
-      'div',
+      "div",
       {
         class: { ui: true, container: true }
       },
-      [
-        h('filter-bar'),
-        this.renderVuetable(h),
-        this.renderPagination(h)
-      ]
-    )
+      [h("filter-bar"), this.renderVuetable(h), this.renderPagination(h)]
+    );
   },
   methods: {
     // render related functions
     renderVuetable(h) {
-      return h(
-        'vuetable',
-        {
-          ref: 'vuetable',
-          props: {
-            apiUrl: this.apiUrl,
-            fields: this.fields,
-            paginationPath: "",
-            perPage: 10,
-            multiSort: true,
-            sortOrder: this.sortOrder,
-            appendParams: this.appendParams,
-            detailRowComponent: this.detailRowComponent,
-          },
-          on: {
-            'vuetable:cell-clicked': this.onCellClicked,
-            'vuetable:pagination-data': this.onPaginationData,
-          },
-          scopedSlots: this.$vnode.data.scopedSlots
-        }
-      )
+      return h("vuetable", {
+        ref: "vuetable",
+        props: {
+          apiUrl: this.apiUrl,
+          fields: this.fields,
+          paginationPath: "",
+          perPage: 10,
+          multiSort: true,
+          sortOrder: this.sortOrder,
+          appendParams: this.appendParams,
+          detailRowComponent: this.detailRowComponent
+        },
+        on: {
+          "vuetable:cell-clicked": this.onCellClicked,
+          "vuetable:pagination-data": this.onPaginationData
+        },
+        scopedSlots: this.$vnode.data.scopedSlots
+      });
     },
     renderPagination(h) {
       return h(
-        'div',
-        { class: {'vuetable-pagination': true, 'ui': true, 'basic': true, 'segment': true, 'grid': true} },
+        "div",
+        {
+          class: {
+            "vuetable-pagination": true,
+            ui: true,
+            basic: true,
+            segment: true,
+            grid: true
+          }
+        },
         [
-          h('vuetable-pagination-info', { ref: 'paginationInfo' }),
-          h('vuetable-pagination', {
-            ref: 'pagination',
+          h("vuetable-pagination-info", { ref: "paginationInfo" }),
+          h("vuetable-pagination", {
+            ref: "pagination",
             on: {
-              'vuetable-pagination:change-page': this.onChangePage
+              "vuetable-pagination:change-page": this.onChangePage
             }
           })
         ]
-      )
+      );
     },
     // ------------------
-    allcap (value) {
-      return value.toUpperCase()
+    allcap(value) {
+      return value.toUpperCase();
     },
-    genderLabel (value) {
-      return value === 'M'
+    genderLabel(value) {
+      return value === "M"
         ? '<span class="ui teal label"><i class="large man icon"></i>Male</span>'
-        : '<span class="ui pink label"><i class="large woman icon"></i>Female</span>'
+        : '<span class="ui pink label"><i class="large woman icon"></i>Female</span>';
     },
-    formatNumber (value) {
-      return accounting.formatNumber(value, 2)
+    formatNumber(value) {
+      return accounting.formatNumber(value, 2);
     },
-    formatDate (value, fmt = 'D MMM YYYY') {
-      return (value == null)
-        ? ''
-        : moment(value, 'YYYY-MM-DD').format(fmt)
+    formatDate(value, fmt = "D MMM YYYY") {
+      return value == null ? "" : moment(value, "YYYY-MM-DD").format(fmt);
     },
-    onPaginationData (paginationData) {
-      this.$refs.pagination.setPaginationData(paginationData)
-      this.$refs.paginationInfo.setPaginationData(paginationData)
+    onPaginationData(paginationData) {
+      this.$refs.pagination.setPaginationData(paginationData);
+      this.$refs.paginationInfo.setPaginationData(paginationData);
     },
-    onChangePage (page) {
-      this.$refs.vuetable.changePage(page)
+    onChangePage(page) {
+      this.$refs.vuetable.changePage(page);
     },
-    onCellClicked (data, field, event) {
-      console.log('cellClicked: ', field.name)
-      this.$refs.vuetable.toggleDetailRow(data.id)
+    onCellClicked(data, field, event) {
+      console.log("cellClicked: ", field.name);
+      this.$refs.vuetable.toggleDetailRow(data.id);
     },
-    onFilterSet (filterText) {
-      this.appendParams.filter = filterText
-      Vue.nextTick( () => this.$refs.vuetable.refresh() )
+    onFilterSet(filterText) {
+      this.appendParams.filter = filterText;
+      Vue.nextTick(() => this.$refs.vuetable.refresh());
     },
-    onFilterReset () {
-      delete this.appendParams.filter
-      Vue.nextTick( () => this.$refs.vuetable.refresh() )
+    onFilterReset() {
+      delete this.appendParams.filter;
+      Vue.nextTick(() => this.$refs.vuetable.refresh());
     }
   }
-}
+};
 </script>
